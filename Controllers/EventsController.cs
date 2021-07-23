f839b2d9d5a1d03fe5f5521be99bdce66969d6cd
Mike <mike.sierra.7950@gmail.com>
2021-07-23 11:05:06 -0400

    FINAL: Final changes made to include ViewBag.Title and program functions as desired

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -53,7 +53,7 @@ namespace CodingEvents.Controllers
         public IActionResult Edit(int eventId)
         {
             ViewBag.eventToEdit = EventData.GetById(eventId);
-            ViewBag.Title = "Edit Event NAME (id=ID)";
+            ViewBag.Title = $"Edit Event \"{ViewBag.eventToEdit.Name}\" (id = \"{ViewBag.eventToEdit.Id}\")";
             return View();
         }
         [HttpPost("/Events/Edit")]
