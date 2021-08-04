aa309d0f7251ceac69f47846b02b037400a2ce73
Mike <mike.sierra.7950@gmail.com>
2021-08-04 14:10:19 -0400

    Exercise Completed

---
+++ Controllers/EventCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext _context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategories = _context.EventCategories.ToList();
+            return View(eventCategories);
+        }
+    }
+}
--- Data/EventDbContext.cs
+++ Data/EventDbContext.cs
@@ -10,9 +10,7 @@ namespace CodingEvents.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
-
-        public EventDbContext()
-        {}
+        public DbSet<EventCategory> EventCategories { get; set; }
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
         {}
     }
